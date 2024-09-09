96b2b60f8947c922364f4164abc42c58b10586c7
otiisaac199 <otichinazaisaac19@gmail.com>
2024-09-09 11:24:32 +0100

    fixed the play button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -38,22 +38,19 @@ const Player = () => {
             src={assets.prev_icon}
             alt=""
           />
-          {playStatus ? (
-            <img
-              onClick={pause}
-              className="w-4 cursor-pointer"
-              src={assets.pause_icon}
-              alt=""
-            />
-          ) : (
-            <img
-              onClick={play}
-              className="w-4 cursor-pointer"
-              src={assets.play_icon}
-              alt=""
-            />
-          )}
 
+          <img
+            onClick={play}
+            className="w-4 cursor-pointer"
+            src={assets.play_icon}
+            alt=""
+          />
+          <img
+            onClick={pause}
+            className="w-4 cursor-pointer"
+            src={assets.pause_icon}
+            alt=""
+          />
           <img
             onClick={next}
             className="w-4 cursor-pointer"
